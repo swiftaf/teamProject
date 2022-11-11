a424cd029ca801c538a6c6fa50cfbde908e22b2c
z3301 <scoopnisker@gmail.com>
2022-11-11 12:58:05 -0500

    working game center login

--- Plasticiti/ViewController.swift
+++ Plasticiti/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        authenticateUser()
     }
-
-
+    
+    private func authenticateUser(){
+        let player = GKLocalPlayer.local
+        
+        player.authenticateHandler = { vc, error in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            self.present(vc!, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func showAchievements(_ sender: Any) {
+    }
+    @IBAction func showLeaderboards(_ sender: Any) {
+    }
+    @IBAction func unlockAchievement(_ sender: Any) {
+    }
+    @IBAction func submitScore(_ sender: Any) {
+    }
 }
 
