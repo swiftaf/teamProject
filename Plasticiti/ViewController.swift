f732209691e528762b3d313a5bb8fe31f5c1d6ff
z3301 <scoopnisker@gmail.com>
2022-11-12 13:11:24 -0500

    working leaderboard

--- Plasticiti/ViewController.swift
+++ Plasticiti/ViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 import GameKit
+import SwiftUI
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, GKGameCenterControllerDelegate {
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +32,59 @@ class ViewController: UIViewController {
                 return
             }
             if let vc = vc {
+                GKAccessPoint.shared.location = .topLeading
+                GKAccessPoint.shared.showHighlights = true
+                GKAccessPoint.shared.isActive = true
                 self.present(vc, animated: true, completion: nil)
             }
         }
     }
     
     @IBAction func showAchievements(_ sender: Any) {
+        let vc = GKGameCenterViewController()
+        vc.gameCenterDelegate = self
+        vc.viewState = .achievements
+        present(vc, animated: true, completion: nil)
     }
     @IBAction func showLeaderboards(_ sender: Any) {
+        let vc = GKGameCenterViewController()
+        vc.gameCenterDelegate = self
+        vc.viewState = .leaderboards
+        vc.leaderboardIdentifier = "WordleysWon"
+        present(vc, animated: true, completion: nil)
     }
     @IBAction func unlockAchievement(_ sender: Any) {
+        let achievment = GKAchievement(identifier: "solved50wordleys")
+        achievment.percentComplete = 100
+        achievment.showsCompletionBanner = true
+        GKAchievement.report([achievment]) { error
+            in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            print("done!")
+        }
     }
     @IBAction func submitScore(_ sender: Any) {
+        let score = GKScore(leaderboardIdentifier: "WordleysWon")
+        score.value = 100
+        GKScore.report([score]) { error
+            in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            print("done!")
+        }
     }
+    
 }
 
+
+
+//extension ViewController: GKGameCenterControllerDelegate {
+//    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+//        gameCenterViewController.dismiss(animated: true, completion: nil)
+//    }
+//}
